4153a332057faa14aadc1d290c4d811652df33f1
bbennett <tbbennett1@gmail.com>
2019-12-02 16:08:29 -0800

    can now receive current temp for given city

--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"net/http"
+	"encoding/json"
+	"strings"
+)
+
 func main() {
-    println("hello!")
+  http.HandleFunc("/", hello)
+
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+		data, err := query(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("hellothere!"))
+}
+
+func query(city string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=565b5acb7bbdb846ee781c81c44b9549&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var data weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return weatherData{}, err
+	}
+
+	return data, nil
+}
+
+// struct to capture city name and temp (in Kelvin)
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
 }
